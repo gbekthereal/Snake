33fb4bbe5ef8c8633e4be4e4d85daf479395df57
Giannis Bekakos <johnbekakos@gmail.com>
2023-09-17 17:11:33 +0300

    Add project files.

---
+++ Snake/GridValue.cs
@@ -0,0 +1,9 @@
+﻿namespace Snake
+{
+    public enum GridValue
+    {
+        Empty, Snake, Food, Outside
+    }
+
+
+}
---
+++ Snake/MainWindow.xaml.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Snake
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private readonly Dictionary<GridValue, ImageSource> gridValToImage = new()
+        {
+            { GridValue.Empty, Images.Empty },
+            { GridValue.Snake, Images.Body },
+            { GridValue.Food, Images.Food }
+        };
+
+        private readonly Dictionary<Direction, int> dirToRotation = new ()
+        {
+            { Direction.Up, 0 },
+            { Direction.Right, 90 },
+            { Direction.Down, 180 },
+            { Direction.Left, 270}
+        };
+
+        private readonly int rows=20, cols=20;
+        private readonly Image[,] gridImages;
+        private GameState gameState;
+        private bool gameRunning;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            gridImages = SetupGrid();
+            gameState = new GameState(rows, cols);
+        }
+
+        private async Task RunGame()
+        {
+            Draw();
+            await ShowCountDown();
+            Overlay.Visibility= Visibility.Hidden;
+            await GameLoop();
+            await ShowGameOver();
+            gameState = new GameState(rows, cols);
+        }
+
+        private async void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (Overlay.Visibility == Visibility.Visible)
+            {
+                e.Handled= true;
+            }
+
+            if (!gameRunning)
+            {
+                gameRunning = true;
+                await RunGame();
+                gameRunning = false;
+            }
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (gameState.GameOver) return;
+            switch (e.Key)
+            {
+                case Key.Left:
+                    gameState.ChangeDirection(Direction.Left);
+                    break;
+                case Key.Right:
+                    gameState.ChangeDirection(Direction.Right);
+                    break;
+                case Key.Up:
+                    gameState.ChangeDirection(Direction.Up);
+                    break;
+                case Key.Down:
+                    gameState.ChangeDirection(Direction.Down);
+                    break;
+                }
+        }
+
+        private async Task GameLoop()
+        {
+            while(!gameState.GameOver)
+            {
+                await Task.Delay(120);
+                gameState.Move();
+                Draw();
+            }
+        }
+
+        private Image[,] SetupGrid()
+        {
+            Image[,] images= new Image[rows, cols];
+            GameGrid.Rows = rows;
+            GameGrid.Columns= cols;
+            GameGrid.Width = GameGrid.Height * (cols / (double)rows);
+
+            for (int i=0; i<rows; i++)
+            {
+                for (int j=0; j<cols; j++)
+                {
+                    Image image = new Image { Source = Images.Empty, RenderTransformOrigin = new Point(0.5, 0.5) };
+                    images[i, j] = image;
+                    GameGrid.Children.Add(image);
+                }
+            }
+            return images;
+        }
+
+        private void Draw()
+        {
+            DrawGrid();
+            DrawShakeHead();
+            ScoreText.Text = $"SCORE {gameState.Score}";
+        }
+
+        private void DrawGrid()
+        {
+            for (int i=0; i<rows; i++)
+            {
+                for (int j=0; j<cols; j++)
+                {
+                    GridValue gridVal = gameState.Grid[i, j];
+                    gridImages[i, j].Source = gridValToImage[gridVal];
+                    gridImages[i, j].RenderTransform = Transform.Identity;
+                }
+            }
+        }
+
+        private void DrawShakeHead()
+        {
+            Position headPos = gameState.HeadPosition();
+            Image image = gridImages[headPos.Row, headPos.Col];
+            image.Source = Images.Head;
+
+            int rotation = dirToRotation[gameState.Dir];
+            image.RenderTransform = new RotateTransform(rotation);
+        }
+
+        private async Task DrawDeadSnake()
+        {
+            List<Position> positions= new List<Position>(gameState.SnakePositions());
+
+            for (int i=0; i<positions.Count; i++)
+            {
+                Position pos = positions[i];
+                ImageSource source = (i == 0) ? Images.DeadHead : Images.DeadBody;
+                gridImages[pos.Row, pos.Col].Source = source;
+                await Task.Delay(50);
+            }
+
+        }
+
+        private async Task ShowCountDown()
+        {
+            for (int i=3; i>=1; i--)
+            {
+                OverlayText.Text = i.ToString();
+                await Task.Delay(500);
+            }
+        }
+
+        private async Task ShowGameOver()
+        {
+            await DrawDeadSnake();
+            await Task.Delay(1000);
+            Overlay.Visibility= Visibility.Visible;
+            OverlayText.Text = "PRESS ANY KEY TO START";
+        }
+    }
+}
